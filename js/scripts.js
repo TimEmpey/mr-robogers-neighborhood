1acc8515eb2622ddb621149da67f2a1bc9a51913
Tim Empey <timothyempey@gmail.com>
2022-08-19 14:32:43 -0700

    add handleSubmission() to display results of array

--- js/scripts.js
+++ js/scripts.js
@@ -5,37 +5,29 @@ function beepBoop(number) {
   const newArray = []
   for (let i = 0; i <= number; i++) {
     if (i.toString().includes(3)) {
-      console.log("pass3");
       newArray.push("Won't you be my neighbor")
     } else if (i.toString().includes(2)) {
-      console.log("pass2");
       newArray.push("Boop!")
     } else if(i.toString().includes(1)) {
-      console.log("pass1");
       newArray.push("Beep!")
     } else {
-      console.log("passNum");
       newArray.push(i);
     }
   }
 
     return newArray;
   }
-  
 
 //* UI Logic
 
 function handleSubmission() {
   event.preventDefault();
-  const submitButton = document.getElementById("submit-button");
-  const form = document.querySelector("form")
-  document.getElementById("arrayInput").innerText=numArrayList
-  
-  form.addEventListener("submit", handleSubmission);
-  form.addEventListener("reset", resetForm)
-
-  function handleSubmission(event){
-    event.preventDefault();
-  }
+  const number = document.getElementById("num").value;
+  const beepBoopNew = beepBoop(number);
+  document.getElementById("arrayInput").innerText = beepBoopNew;
 };
+
+window.addEventListener("load", function() {
+  document.querySelector("form#numberScan").addEventListener("submit", handleSubmission);
+});
 
