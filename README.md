1e3b114239b3e1891b1e19f942d71cd91bfee026
Tim Empey <timothyempey@gmail.com>
2022-08-19 12:10:58 -0700

    make beepBoop() pass test 3

--- README.md
+++ README.md
@@ -6,8 +6,16 @@ const number = 8
 beepBoop(number);
 Expected Output: [8]
 
-Test: Should return inputer number from 0-inputed number
+Test: Should return array from 0-inputed number
 Code:
 const number = 8
 beepBoop(number);
 Expected Output: [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+Test: Should return array from 0-input but mark values containing 1,2, or 3 as pass, pass1, pass2
+Code
+const number = 8
+beepBoop(number);
+Expected Output: 
+[0, 1, 2, 3, 4, 5, 6, 7, 8]
+'pass1' 'pass2' 'pass3'
--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,43 @@
+//* Business Logic
+
 function beepBoop(number) {
-  numArray = [number];
-  const numArrayList = []
+  const numArray = [number];
+  const newArray = []
   for (let i = 0; i <= number; i++) {
-    numArrayList.push(i)
+    if (i.toString().includes(3)) {
+      console.log("pass3");
+      newArray.push("Won't you be my neighbor")
+    } else if (i.toString().includes(2)) {
+      console.log("pass2");
+      newArray.push("Boop!")
+    } else if(i.toString().includes(1)) {
+      console.log("pass1");
+      newArray.push("Beep!")
+    } else {
+      console.log("passNum");
+      newArray.push(i);
+    }
   }
-  return numArrayList;
-}
+
+    return newArray;
+  }
+  
+  
+  //return numArrayList;
+
+//* UI Logic
+
+// function handleSubmission() {
+//   event.preventDefault();
+//   const submitButton = document.getElementById("submit-button");
+//   const form = document.querySelector("form")
+//   document.getElementById("arrayInput").innerText=numArrayList
+  
+//   form.addEventListener("submit", handleSubmission);
+//   form.addEventListener("reset", resetForm)
+
+//   function handleSubmission(event){
+//     event.preventDefault();
+//   }
+// };
+
